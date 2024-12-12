430bbb452ede906f00de77266174f47e3bd1a5ad
RizkyAchmadFadilah076 <braviabmb27@gmail.com>
2024-12-13 01:57:13 +0700

    coverbuku

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    @hasrole('pustakawan')
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Book') }}
@@ -6,7 +7,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
@@ -102,4 +103,5 @@
             </div>
         </div>
     </div>
+    @endhasrole
 </x-app-layout>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
+        
+
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Dashboard Admin') }}
         </h2>
     </x-slot>
 
@@ -9,7 +11,25 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Selamat Datang, ".Auth()->user()->name." !") }}
+                <div class="bg-white p-6 rounded-lg shadow">
+                    <h3 class="text-xl font-semibold text-gray-800">Jumlah Buku</h3>
+                    <p class="mt-4 text-gray-600">1 Buku</p>
+                </div>
+
+                <!-- Card 2 -->
+                <div class="bg-white p-6 rounded-lg shadow">
+                    <h3 class="text-xl font-semibold text-gray-800">Jumlah Anggota</h3>
+                    <p class="mt-4 text-gray-600">1 Anggota</p>
+                </div>
+
+                <!-- Card 3 -->
+                <div class="bg-white p-6 rounded-lg shadow">
+                    <h3 class="text-xl font-semibold text-gray-800">Jumlah Peminjaman</h3>
+                    <p class="mt-4 text-gray-600">0 Buku</p>
+                </div>
+            </div>
+        </main>
+    </div>
                 </div>
             </div>
         </div>
